c14a380c22265568784bd7c762cb057be93e580a
PeterWrighten <peterwrighten@gmail.com>
2021-11-08 20:41:38 +0900

    update UDP APP

--- SocketProgramming1/UDP_Client.py
+++ SocketProgramming1/UDP_Client.py
@@ -1,11 +1,13 @@
-from socket import *
+#!/usr/bin/python3
+
+import socket
 
 
-serverName = 'hostname'
+serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
-message = raw_input('Input lowercase sentence: ')
-clientSocket.sendto(message, (serverName, serverPort))
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+message = input('Input lowercase sentence: ')
+clientSocket.sendto(message.encode('utf-8'), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
+print(modifiedMessage.decode('utf-8'))
 clientSocket.close()
